fix masking nan problem

Transformer.py
@@ -15,11 +15,11 @@ import utils
 import time
 
 MODEL_DIM = 32
-MAX_LEN = 15
+MAX_LEN = 12
 N_LAYER = 3
 N_HEAD = 4
 DROP_RATE = 0.1
-T = []
+
 
 class Transformer:
     def __init__(self, model_dim, max_len, n_layer, n_head, n_vocab, drop_rate=0.1):
@@ -33,14 +33,14 @@ class Transformer:
         self.tfx = tf.placeholder(tf.int32, [None, max_len])            # [n, step]
         self.tfy = tf.placeholder(tf.int32, [None, max_len+1])          # [n, step+1]
 
-        embeddings = tf.Variable(tf.random_normal((n_vocab, model_dim), 0., 0.01))           # [n_vocab, dim]
-        x_embedded = tf.nn.embedding_lookup(embeddings, self.tfx) + self.position_embedding()   # [n, step, dim]
-        y_embedded = tf.nn.embedding_lookup(embeddings, self.tfy[:, :-1]) + self.position_embedding()  # [n, step, dim]
+        embeddings = tf.Variable(tf.random_normal((n_vocab, model_dim), 0., 0.01))                      # [n_vocab, dim]
+        x_embedded = tf.nn.embedding_lookup(embeddings, self.tfx) + self.position_embedding()           # [n, step, dim]
+        y_embedded = tf.nn.embedding_lookup(embeddings, self.tfy[:, :-1]) + self.position_embedding()   # [n, step, dim]
 
         encoded_z = self._build_encoder(x_embedded)
         decoded_z = self._build_decoder(y_embedded, encoded_z)
 
-        self.logits = tf.layers.dense(decoded_z, n_vocab)                # [n, step, n_vocab]
+        self.logits = tf.layers.dense(decoded_z, n_vocab)                           # [n, step, n_vocab]
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
             labels=self.tfy[:, 1:], logits=self.logits)
         self.loss = tf.reduce_mean(cross_entropy)
@@ -72,23 +72,23 @@ class Transformer:
             mask = tf.tile(mask, [self.n_head, 1, 1])                           # repeat for n_head
             score = tf.where(mask, score, tf.fill(tf.shape(score), -np.inf))    # make softmax not select padded value
         attention = tf.nn.softmax(score, axis=-1)                               # [h*n, q_step, step]
-        attention = tf.where(tf.is_nan(attention), tf.zeros_like(attention), attention)     # replace nan caused by all padding by 0
+        attention = tf.where(tf.is_nan(attention), tf.zeros_like(attention), attention)  # replace nan caused by all padding by 0
         attention = tf.layers.dropout(attention, rate=self.drop_rate, training=self.training)
-        context = tf.matmul(attention, v)           # [h*n, q_step, step] @ [h*n, step, dv] = [h*n, q_step, dv]
+        context = tf.matmul(attention, v)                   # [h*n, q_step, step] @ [h*n, step, dv] = [h*n, q_step, dv]
         return attention, context
 
     def multi_head(self, query, key, value, mask=None):
         head_dim = self.model_dim // self.n_head
-        q = tf.layers.dense(query, self.n_head * head_dim)          # [n, q_step, h_dim*h]
-        k = tf.layers.dense(key, self.n_head * head_dim)            # [n, step, h_dim*h]
-        v = tf.layers.dense(value, self.n_head * head_dim)          # [n, step, h_dim*h]
-        q_ = tf.concat(tf.split(q, self.n_head, axis=2), axis=0)    # [h*n, q_step, h_dim]
-        k_ = tf.concat(tf.split(k, self.n_head, axis=2), axis=0)    # [h*n, step, h_dim]
-        v_ = tf.concat(tf.split(v, self.n_head, axis=2), axis=0)    # [h*n, step, h_dim]
+        q = tf.layers.dense(query, self.n_head * head_dim)              # [n, q_step, h_dim*h]
+        k = tf.layers.dense(key, self.n_head * head_dim)                # [n, step, h_dim*h]
+        v = tf.layers.dense(value, self.n_head * head_dim)              # [n, step, h_dim*h]
+        q_ = tf.concat(tf.split(q, self.n_head, axis=2), axis=0)        # [h*n, q_step, h_dim]
+        k_ = tf.concat(tf.split(k, self.n_head, axis=2), axis=0)        # [h*n, step, h_dim]
+        v_ = tf.concat(tf.split(v, self.n_head, axis=2), axis=0)        # [h*n, step, h_dim]
 
         attention, context = self.scaled_dot_product_attention(q_, k_, v_, mask)
         self.attentions.append(tf.transpose(tf.split(attention, self.n_head, axis=0), (1, 0, 2, 3))) # [n, h, q_step, step]
-        o = tf.concat(tf.split(context, self.n_head, axis=0), axis=2)  # [n, q_step, h*h_dim=model_dim]
+        o = tf.concat(tf.split(context, self.n_head, axis=0), axis=2)   # [n, q_step, h*h_dim=model_dim]
         o = tf.layers.dense(o, self.model_dim, use_bias=False)
         o = tf.layers.dropout(o, rate=self.drop_rate, training=self.training)
         return o
@@ -107,7 +107,7 @@ class Transformer:
         pe = pos / np.power(10000, 2. * np.arange(self.model_dim)[None, :] / self.model_dim)  # [max_len, model_dim]
         pe[:, 0::2] = np.sin(pe[:, 0::2])
         pe[:, 1::2] = np.cos(pe[:, 1::2])
-        pe = pe[None, :, :]                         # [1, max_len, model_dim]    for batch adding
+        pe = pe[None, :, :]                                             # [1, max_len, model_dim]    for batch adding
         return tf.constant(pe, dtype=tf.float32)
 
     @staticmethod
@@ -118,7 +118,7 @@ class Transformer:
     def output_mask(self, seqs):
         pad_mask = self.pad_mask(seqs)
         mask = tf.constant(~np.triu(np.ones((self.max_len, self.max_len), dtype=np.bool), 1))
-        mask = tf.tile(tf.expand_dims(mask, axis=0), [tf.shape(seqs)[0], 1, 1])     # [n, step, step]
+        mask = tf.tile(tf.expand_dims(mask, axis=0), [tf.shape(seqs)[0], 1, 1])                     # [n, step, step]
         return tf.where(mask, pad_mask, tf.zeros_like(pad_mask))
 
     def translate(self, src, v2i, i2v):
@@ -148,7 +148,6 @@ t0 = time.time()
 for t in range(1000):
     bi = np.random.randint(0, len(x), size=64)
     bx, by = utils.pad_zero(x[bi], max_len=MAX_LEN), utils.pad_zero(y[bi], max_len=MAX_LEN+1)
-    # tt = model.sess.run(T, {model.tfx: bx, model.tfy: by, model.training: True})
     _, loss_ = model.sess.run([model.train_op, model.loss], {model.tfx: bx, model.tfy: by, model.training: True})
     if t % 50 == 0:
         logits_ = model.sess.run(model.logits, {model.tfx: bx[:1, :], model.tfy: by[:1, :], model.training: False})