d19664f8fd7e14a5979a0b6e341cfb29d346a635
morvanzhou <morvanzhou@gmail.com>
2018-12-28 19:30:01 +0800

    update

--- utils.py
+++ utils.py
@@ -64,7 +64,7 @@ def generate_dist_data():
     return emb_dist1, emb_dist2, frequent_words, infrequent_words
 
 
-def download_mrpc(save_dir="./MRPC/", proxy=None):
+def maybe_download_mrpc(save_dir="./MRPC/", proxy=None):
     train_url = 'https://s3.amazonaws.com/senteval/senteval_data/msr_paraphrase_train.txt'
     test_url = 'https://s3.amazonaws.com/senteval/senteval_data/msr_paraphrase_test.txt'
     os.makedirs(save_dir, exist_ok=True)
@@ -88,7 +88,7 @@ def text_standardize(text):
     return text.strip()
 
 
-def process_mrpc(dir="./MRPC/"):
+def process_mrpc(dir="./MRPC/", go="<GO> ", end=" <EOS>"):
     data = {}
     files = os.listdir(dir)
     for f in files:
@@ -99,17 +99,67 @@ def process_mrpc(dir="./MRPC/"):
     for n in ["train", "test"]:
         for m in ["s1", "s2"]:
             for i in range(len(data[n][m])):
-                data[n][m][i] = "<GO> " + text_standardize(data[n][m][i].lower()) + " <EOS>"
+                data[n][m][i] = go + text_standardize(data[n][m][i].lower()) + end
                 cs = data[n][m][i].split(" ")
                 vocab.update(set(cs))
     v2i = {v: i for i, v in enumerate(vocab, start=1)}
     v2i["<PAD>"] = 0
-    v2i["<SEP>"] = len(v2i)
-    vocab.update(["<PAD>", "<SEP>"])
+    vocab.update(["<PAD>"])
     i2v = {i: v for v, i in v2i.items()}
     for n in ["train", "test"]:
         for m in ["s1", "s2"]:
             data[n][m+"id"] = [[v2i[v] for v in c.split(" ")] for c in data[n][m]]
     max_len = max(
         [len(s) for s in data["train"]["s1id"] + data["train"]["s2id"] + data["test"]["s1id"] + data["test"]["s2id"]])
-    return data, vocab, v2i, i2v, max_len
+    return data, v2i, i2v, max_len
+
+
+def gpt_mrpc(dir="./MRPC"):
+    maybe_download_mrpc(save_dir="./MRPC/")
+    data, v2i, i2v, max_len = process_mrpc(dir, go="<GO> ", end=" <EOS>")
+    max_len = max_len * 2 + 1
+    unsupervised_x = data["train"]["s1id"] + data["train"]["s2id"]
+    unsupervised_x = pad_zero(unsupervised_x, max_len=max_len)
+    supervised_x = [data["train"]["s1id"][i] + data["train"]["s2id"][i] for i in range(len(data["train"]["s1id"]))]
+    supervised_x = pad_zero(supervised_x, max_len=max_len)
+    supervised_label = data["train"]["is_same"]
+    print("task1 example: ", data["train"]["s1"][0])
+    print("task2 example: ", data["train"]["s1"][0] + " " + data["train"]["s2"][0])
+    return v2i, i2v, max_len, unsupervised_x, supervised_x, supervised_label
+
+
+def bert_mrpc(dir="./MRPC/"):
+    maybe_download_mrpc(save_dir="./MRPC/")
+    data, v2i, i2v, max_len = process_mrpc(dir, go="", end=" <SEP>")
+    v2i["<MASK>"] = len(v2i)        # add mask token
+    i2v[v2i["<MASK>"]] = "<MASK>"
+
+    max_len = max_len * 2 + 1
+    x1 = data["train"]["s1id"] + data["train"]["s2id"]
+    len1 = np.array([len(s) for s in x1])
+    x1 = pad_zero(x1, max_len=max_len)
+    x2 = [data["train"]["s1id"][i] + data["train"]["s2id"][i] for i in range(len(data["train"]["s1id"]))]
+    x2 = pad_zero(x2, max_len=max_len)
+    len2 = np.array([len(s1) + len(s2) for s1, s2 in zip(data["train"]["s1id"], data["train"]["s2id"])])
+    y2 = data["train"]["is_same"]
+    print("task1 example: ", data["train"]["s1"][0])
+    print("task2 example: ", data["train"]["s1"][0] + " " + data["train"]["s2"][0])
+
+    seg1 = np.full((len(x1), max_len + 1), 2, np.int32)
+    for i in range(len(data["train"]["s1id"])):
+        si = len(data["train"]["s1id"][i]) + 1  # add 1 task seg
+        seg1[i, :si] = 0
+        si = len(data["train"]["s2id"][i]) + 1  # add 1 task seg
+        seg1[i + len(data["train"]["s1id"]), :si] = 0
+
+    seg2 = np.full((len(data["train"]["s1id"]), max_len + 1), 2, np.int32)
+    for i in range(len(data["train"]["s1id"])):
+        si = 1  # task seg
+        si += len(data["train"]["s1id"][i])
+        seg2[i, :si] = 0
+        si_ = si + len(data["train"]["s2id"][i])
+        seg2[i, si:si_] = 1
+    print("segment1 example: ", seg1[1])
+    print("segment2 example: ", seg2[1])
+    normal_words = set(v2i.keys()).difference(["<SEP>", "<MASK>", "<PAD>"])
+    return x1, x2, y2, seg1, seg2, max_len, v2i, i2v, len1, len2, list(normal_words)
