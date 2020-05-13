5825aa3318e94a5f8d11c45335ff3131defd9af3
morvanzhou <morvanzhou@tencent.com>
2020-05-13 18:55:24 +0800

    update

--- CBOW.py
+++ CBOW.py
@@ -1,7 +1,6 @@
+from tensorflow import keras
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import itertools
+from utils import process_w2v_data, show_w2v_word_embedding
 
 corpus = [
     # numbers
@@ -14,88 +13,74 @@ corpus = [
     "9 3 3 0 1 4 7 8",
     "9 9 8 5 6 7 1 2 3 0 1 0",
 
-    # alphabets
-    "a t g q e h u f",
+    # alphabets, expecting that 9 is close to letters
+    "a t g q e h 9 u f",
     "e q y u o i p s",
-    "q o p l k j o k k o p",
+    "q o 9 p l k j o k k o p",
     "h g y i u t t a e q",
-    "i k d q r e e a d",
-    "o p d g s a f g a",
+    "i k d q r e 9 e a d",
+    "o p d g 9 s a f g a",
     "i u y g h k l a s w",
     "o l u y a o g f s",
     "o p i u y g d a s j d l",
-    "u k i l o l j s",
+    "u k i l o 9 l j s",
     "y g i s h k j l f r f",
+    "i o h n 9 9 d 9 f a 9",
 ]
 
-all_words = [sentence.split(" ") for sentence in corpus]
-all_words = np.array(list(itertools.chain(*all_words)))
-# vocab sort by decreasing frequency for the negative sampling below (nce_loss).
-vocab, v_count = np.unique(all_words, return_counts=True)
-vocab = vocab[np.argsort(v_count)[::-1]]
 
-print("all vocabularies sorted from more frequent to less frequent:\n", vocab)
-v2i = {v: i for i, v in enumerate(vocab)}
-i2v = {i: v for v, i in v2i.items()}
+class CBOW(keras.Model):
+    def __init__(self, v_dim, emb_dim):
+        super().__init__()
+        self.v_dim = v_dim
+        self.embeddings = keras.layers.Embedding(
+            input_dim=v_dim, output_dim=emb_dim,  # [n_vocab, emb_dim]
+            embeddings_initializer=tf.initializers.RandomNormal(0., 0.1),
+        )
 
-# pair data
-pairs = []
-skip_window = 2
-js = list(range(-skip_window, skip_window+1))
-js.remove(0)    # remove center
-for c in corpus:
-    words = c.split(" ")
-    w_idx = [v2i[w] for w in words]
-    for i in range(skip_window, len(w_idx)-skip_window):
-        context = []
-        for j in js:
-            context.append(w_idx[i+j])
-        pairs.append(context + [w_idx[i]])  # (contexts, center) or (feature, target)
-pairs = np.array(pairs)
-print("5 example pairs:\n", pairs[:5])
-x, y = pairs[:, :-1], pairs[:, -1]
+        # noise-contrastive estimation
+        self.nce_w = self.add_weight(
+            name="nce_w", shape=[v_dim, emb_dim],
+            initializer=tf.initializers.TruncatedNormal(0., 1.))  # [n_vocab, emb_dim]
+        self.nce_b = self.add_weight(
+            name="nce_b", shape=(v_dim,),
+            initializer=tf.initializers.Constant(0.1))  # [n_vocab, ]
 
-# inputs
-tfx = tf.placeholder(tf.int32, [None, 2*skip_window])
-tfy = tf.placeholder(tf.int32, [None, ])
+        self.opt = tf.optimizers.Adam(0.01)
 
-emb_dim = 2
-embeddings = tf.Variable(tf.random_uniform((len(vocab), emb_dim), -1., 1.))     # [n_vocab, emb_dim]
-span_embedded = tf.nn.embedding_lookup(embeddings, tfx)                         # [n, span, emb_dim]
-embedded = tf.reduce_mean(span_embedded, axis=1)                                # [n, emb_dim]
+    def call(self, x, training=None, mask=None):
+        # x.shape = [n, skip_window*2]
+        o = self.embeddings(x)          # [n, skip_window*2, emb_dim]
+        o = tf.reduce_mean(o, axis=1)   # [n, emb_dim]
+        return o
 
-# noise-contrastive estimation
-nce_w = tf.Variable(tf.random_uniform((len(vocab), emb_dim), -1., 1.))          # [n_vocab, emb_dim]
-nce_b = tf.Variable(tf.constant(0.1, shape=(len(vocab),)))                      # [n_vocab, ]
+    # negative sampling: take one positive label and num_sampled negative labels to compute the loss
+    # in order to reduce the computation of full softmax
+    def loss(self, x, y, training=None):
+        embedded = self.call(x, training)
+        return tf.reduce_mean(
+            tf.nn.nce_loss(
+                weights=self.nce_w, biases=self.nce_b, labels=tf.expand_dims(y, axis=1),
+                inputs=embedded, num_sampled=5, num_classes=self.v_dim))
 
-# negative sampling: take one positive label and num_sampled negative labels to compute the loss
-# in order to reduce the computation of full softmax
-loss = tf.reduce_mean(
-  tf.nn.nce_loss(weights=nce_w, biases=nce_b, labels=tf.expand_dims(tfy, axis=1),
-                 inputs=embedded, num_sampled=5, num_classes=len(vocab)))
+    def step(self, x, y):
+        with tf.GradientTape() as tape:
+            _loss: tf.Tensor = self.loss(x, y, True)
+            grads = tape.gradient(_loss, self.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.trainable_variables))
+        return _loss.numpy()
 
-train_op = tf.train.AdamOptimizer(0.01).minimize(loss)
-sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
-sess.run(tf.global_variables_initializer())
+
+data = process_w2v_data(corpus, skip_window=2, method="cbow")
+model = CBOW(data.num_word, 2)
+
 
 # training
-for i in range(3000):
-    b_idx = np.random.randint(0, len(x), 8)
-    _, loss_ = sess.run([train_op, loss], {tfx: x[b_idx], tfy: y[b_idx]})
-    if i % 200 == 0:
-        print("loss:", loss_)
+for t in range(2500):
+    bx, by = data.sample(8)
+    loss = model.step(bx, by)
+    if t % 200 == 0:
+        print("step: {} | loss: {}".format(t, loss))
 
 # plotting
-word_emb = sess.run(embeddings)
-for i in range(len(i2v)):
-    bc = "blue"
-    try:
-        int(i2v[i])
-    except ValueError:
-        bc = "red"
-    plt.text(word_emb[i, 0], word_emb[i, 1], s=i2v[i], color="w", weight="bold", backgroundcolor=bc)
-plt.xlim(word_emb[:, 0].min()-.5, word_emb[:, 0].max()+.5)
-plt.ylim(word_emb[:, 1].min()-.5, word_emb[:, 1].max()+.5)
-plt.xticks(())
-plt.yticks(())
-plt.show()
+show_w2v_word_embedding(model, data, "visual_helper/cbow.png")
