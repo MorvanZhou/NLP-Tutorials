ee944c5ce8ed6270183c2802a83c064a5ddea9f1
morvanzhou <morvanzhou@tencent.com>
2020-06-07 01:16:35 +0800

    update

--- BERT_self_mask.py
+++ BERT_self_mask.py
@@ -5,16 +5,8 @@ https://jalammar.github.io/illustrated-bert/
 """
 
 import tensorflow as tf
+from GPT import GPT, train, export_attention
 import utils
-import time
-from GPT import GPT
-import pickle
-import os
-
-MODEL_DIM = 128
-N_LAYER = 4
-BATCH_SIZE = 12
-LEARNING_RATE = 1e-4
 
 
 class BERT(GPT):
@@ -24,21 +16,17 @@ class BERT(GPT):
     def mask(self, seqs):
         """
          abcd--
-        b010011
-        c001011
-        d000111
-        -000011
+        a010011
+        b001011
+        c000111
+        d000011
         -000001
+        -000000
 
-        the mask shows below will see itself by embeddings in the encoder residual layer.
-        # o1 = self.bn[0](attn + xz, training) #
-         abcd--
-        a100011
-        b010011
-        c001011
-        d000101
-        -000011
-        -000001
+        a is a embedding for a-cd
+        b is a embedding for ab-d
+        c is a embedding for abc-
+        later, b embedding will + another b embedding from previous residual input to predict c
         """
         eye = tf.eye(self.max_len+1, batch_shape=[len(seqs)], dtype=tf.float32)[:, 1:, :-1]
         pad = tf.math.equal(seqs, self.padding_idx)
@@ -46,49 +34,14 @@ class BERT(GPT):
         return mask  # [n, 1, step, step]
 
 
-def main():
-    # get and process data
-    data = utils.MRPCData("./MRPC", rows=2000)
-    print("num word: ", data.num_word)
-    model = BERT(
-        model_dim=MODEL_DIM, max_len=data.max_len-1, n_layer=N_LAYER, n_head=4, n_vocab=data.num_word,
-        lr=LEARNING_RATE, max_seg=data.num_seg, drop_rate=0.1, padding_idx=data.pad_id)
-    t0 = time.time()
-    for t in range(5000):
-        seqs, segs, xlen, nsp_labels = data.sample(BATCH_SIZE)
-        loss, pred = model.step(seqs[:, :-1], segs[:, :-1], seqs[:, 1:], nsp_labels)
-        if t % 50 == 0:
-            pred = pred[0].numpy().argmax(axis=1)
-            t1 = time.time()
-            print(
-                "\n\nstep: ", t,
-                "| time: %.2f" % (t1 - t0),
-                "| loss: %.3f" % loss.numpy(),
-                "\n| tgt: ", " ".join([data.i2v[i] for i in seqs[0, 1:][:xlen[0].sum()+1]]),
-                "\n| prd: ", " ".join([data.i2v[i] for i in pred[:xlen[0].sum()+1]]),
-                )
-            t0 = t1
-    os.makedirs("./visual_helper/bert", exist_ok=True)
-    model.save_weights("./visual_helper/bert/model.ckpt")
-
-
-def export_attention():
-    data = utils.MRPCData("./MRPC", rows=2000)
-    print("num word: ", data.num_word)
-    model = BERT(
-        model_dim=MODEL_DIM, max_len=data.max_len-1, n_layer=N_LAYER, n_head=4, n_vocab=data.num_word,
-        lr=LEARNING_RATE, max_seg=data.num_seg, drop_rate=0.1, padding_idx=data.pad_id)
-    model.load_weights("./visual_helper/bert/model.ckpt")
-
-    # save attention matrix for visualization
-    seqs, segs, xlen, nsp_labels = data.sample(32)
-    model(seqs[:, :-1], segs[:, :-1], False)
-    data = {"src": [[data.i2v[i] for i in seqs[j]] for j in range(len(seqs))], "attentions": model.attentions}
-    with open("./visual_helper/bert_attention_matrix.pkl", "wb") as f:
-        pickle.dump(data, f)
-
-
 if __name__ == "__main__":
-    main()
-    export_attention()
+    MODEL_DIM = 128
+    N_LAYER = 4
+    LEARNING_RATE = 1e-4
+    d = utils.MRPCData("./MRPC", 2000)
+    m = BERT(
+        model_dim=MODEL_DIM, max_len=d.max_len - 1, n_layer=N_LAYER, n_head=4, n_vocab=d.num_word,
+        lr=LEARNING_RATE, max_seg=d.num_seg, drop_rate=0.2, padding_idx=d.pad_id)
+    train(m, d, step=5000, name="bert")
+    export_attention(m, d, "bert")
 
