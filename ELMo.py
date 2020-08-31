34ebf21a09f54c8a088dbb206f02db1fce121b15
morvanzhou <morvanzhou@tencent.com>
2020-08-31 19:03:02 +0800

    update

--- ELMo.py
+++ ELMo.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
 import tensorflow as tf
+import numpy as np
 import utils
 import time
 import os
@@ -8,85 +9,80 @@ import os
 class ELMo(keras.Model):
     def __init__(self, v_dim, emb_dim, units, n_layers, lr):
         super().__init__()
+        self.n_layers = n_layers
+        self.units = units
 
         # encoder
         self.word_embed = keras.layers.Embedding(
             input_dim=v_dim, output_dim=emb_dim,  # [n_vocab, emb_dim]
-            embeddings_initializer=keras.initializers.RandomNormal(0., 0.01),
+            embeddings_initializer=keras.initializers.RandomNormal(0., 0.001),
             mask_zero=True,
         )
-        f_rnn_cells = [keras.layers.LSTMCell(units) for _ in range(n_layers)]
-        self.f_stacked_lstm = keras.layers.StackedRNNCells(f_rnn_cells)
-        self.lstm_forward = keras.layers.RNN(self.f_stacked_lstm, return_sequences=True)
+        # forward lstm
+        self.fs = [keras.layers.LSTM(units, return_sequences=True) for _ in range(n_layers)]
+        self.f_logits = keras.layers.Dense(v_dim)
+        # backward lstm
+        self.bs = [keras.layers.LSTM(units, return_sequences=True, go_backwards=True) for _ in range(n_layers)]
+        self.b_logits = keras.layers.Dense(v_dim)
 
-        b_rnn_cells = [keras.layers.LSTMCell(units) for _ in range(n_layers)]
-        self.b_stacked_lstm = keras.layers.StackedRNNCells(b_rnn_cells)
-        self.lstm_backward = keras.layers.RNN(self.b_stacked_lstm, return_sequences=True, go_backwards=True)
-        self.word_pred = keras.layers.Dense(v_dim)
-
-        self.cross_entropy = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.cross_entropy1 = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.cross_entropy2 = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.opt = keras.optimizers.Adam(lr)
 
-    def __call__(self, seqs):
+    def call(self, seqs):
         embedded = self.word_embed(seqs)        # [n, step, dim]
         """
         0123    forward
         1234    forward predict
          1234   backward 
          0123   backward predict
-         123    forward & backward concat overall prediction
         """
         mask = self.word_embed.compute_mask(seqs)
-        f = self.lstm_forward(
-            embedded[:, :-1],
-            mask=mask[:, :-1],
-            initial_state=self.f_stacked_lstm.get_initial_state(
-                batch_size=embedded.shape[0], dtype=tf.float32))      # [n, step-1, dim]
-        b = self.lstm_backward(
-            embedded[:, 1:],
-            mask=mask[:, 1:],
-            initial_state=self.f_stacked_lstm.get_initial_state(
-                batch_size=embedded.shape[0], dtype=tf.float32))      # [n, step-1, dim]
-        b_reverse = tf.reverse(b, axis=[1])
-        o = tf.concat(
-            (f[:, :-1],             # [012]3 to predict 123
-             b_reverse[:, 1:]),     # [432]1 -> 1[234] to predict 123
-            axis=-1)       # [n, step-2, 2*dim]
-        word_logits = self.word_pred(o)                     # [n, step-2, vocab] to predict 123
-        return word_logits, (f, b_reverse)
+        fxs, bxs = [embedded[:, :-1]], [embedded[:, 1:]]
+        for fl, bl in zip(self.fs, self.bs):
+            fx = fl(
+                fxs[-1], mask=mask[:, :-1], initial_state=fl.get_initial_state(fxs[-1])
+            )           # [n, step-1, dim]
+            bx = bl(
+                bxs[-1], mask=mask[:, 1:], initial_state=bl.get_initial_state(bxs[-1])
+            )  # [n, step-1, dim]
+            fxs.append(fx)      # predict 1234
+            bxs.append(tf.reverse(bx, axis=[1]))    # predict 0123
+        return fxs, bxs
 
     def step(self, seqs):
         with tf.GradientTape() as tape:
-            word_logits, _ = self(seqs)
-            loss = self.cross_entropy(seqs[:, 1:-1], word_logits)
+            fxs, bxs = self.call(seqs)
+            fo, bo = self.f_logits(fxs[-1]), self.b_logits(bxs[-1])
+            loss = (self.cross_entropy1(seqs[:, 1:], fo) + self.cross_entropy2(seqs[:, :-1], bo))/2
         grads = tape.gradient(loss, self.trainable_variables)
         self.opt.apply_gradients(zip(grads, self.trainable_variables))
-        return loss, word_logits
+        return loss, (fo, bo)
 
     def get_emb(self, seqs):
-        _, (f, b_reverse) = self(seqs)
-        word_emb = tf.concat(
-            (f[:, 1:],  # 0[123]
-             b_reverse[:, :-1]),  # 4[321] -> [123]4
-            axis=-1
-        )
-        return word_emb
+        fxs, bxs = self.call(seqs)
+        xs = [tf.concat((f[:, :-1, :], b[:, 1:, :]), axis=2).numpy() for f, b in zip(fxs, bxs)]
+        for x in xs:
+            print("layers shape=", x.shape)
+        return np.mean(xs)
 
 
 def train(model, data, step):
     t0 = time.time()
     for t in range(step):
         seqs = data.sample(BATCH_SIZE)
-        loss, pred = model.step(seqs)
-        if t % 100 == 0:
-            pred = pred[0].numpy().argmax(axis=1)
+        loss, (fo, bo) = model.step(seqs)
+        if t % 80 == 0:
+            fp = fo[0].numpy().argmax(axis=1)
+            bp = bo[0].numpy().argmax(axis=1)
             t1 = time.time()
             print(
                 "\n\nstep: ", t,
                 "| time: %.2f" % (t1 - t0),
                 "| loss: %.3f" % loss.numpy(),
-                "\n| tgt: ", " ".join([data.i2v[i] for i in seqs[0, 1:] if i != data.pad_id]),
-                "\n| prd: ", " ".join([data.i2v[i] for i in pred if i != data.pad_id]),
+                "\n| tgt: ", " ".join([data.i2v[i] for i in seqs[0] if i != data.pad_id]),
+                "\n| f_prd: ", " ".join([data.i2v[i] for i in fp if i != data.pad_id]),
+                "\n| b_prd: ", " ".join([data.i2v[i] for i in bp if i != data.pad_id]),
                 )
             t0 = t1
     os.makedirs("./visual/models/elmo", exist_ok=True)
@@ -101,12 +97,11 @@ def export_w2v(model, data):
 
 if __name__ == "__main__":
     UNITS = 256
-    EMB_DIM = 128
     N_LAYERS = 2
     BATCH_SIZE = 16
-    LEARNING_RATE = 1e-4
-    d = utils.MRPCSingle("./MRPC", rows=100)
+    LEARNING_RATE = 2e-3
+    d = utils.MRPCSingle("./MRPC", rows=2000)
     print("num word: ", d.num_word)
-    m = ELMo(d.num_word, emb_dim=EMB_DIM, units=UNITS, n_layers=N_LAYERS, lr=LEARNING_RATE)
+    m = ELMo(d.num_word, emb_dim=UNITS, units=UNITS, n_layers=N_LAYERS, lr=LEARNING_RATE)
     train(m, d, 10000)
     export_w2v(m, d)
