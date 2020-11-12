fb77ed9366f1dc2575acd12513ee50b5447912dd
wifi <761681664@qq.com>
2020-11-12 16:45:45 +0800

    添加署名和自述文件

--- simple_realize/cnn-lm.py
+++ simple_realize/cnn-lm.py
@@ -1,4 +1,7 @@
 # [Sequence to Sequence Learning with Neural Networks](https://papers.nips.cc/paper/5346-sequence-to-sequence-learning-with-neural-networks.pdf)
+'''
+created by YuYang github.com/W1Fl
+'''
 import tensorflow as tf
 from tensorflow import keras
 import utils
