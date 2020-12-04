304e3617e5a068cf34e833dd7b330f8023ef92c2
morvanzhou <morvanzhou@tencent.com>
2020-12-04 11:12:54 +0800

    update

--- utils.py
+++ utils.py
@@ -246,3 +246,13 @@ def process_w2v_data(corpus, skip_window=2, method="skip_gram"):
     return Dataset(x, y, v2i, i2v)
 
 
+def set_soft_gpu(soft_gpu):
+    import tensorflow as tf
+    if soft_gpu:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
